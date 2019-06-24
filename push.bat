233681db76b433614e3c2052084d4d3d58cd346d
StefanSchmelz <35258671+StefanSchmelz@users.noreply.github.com>
2019-06-24 20:16:49 +0200

    Changed <Charname>

--- push.bat
+++ push.bat
@@ -2,7 +2,7 @@
 set path=C:\Users\<username>\rpgframework\player\myself\shadowrun
 set message="Changed <Charname>"
 
-cd $path
+cd %path%
 git add -A
-git commit -m $message
+git commit -m %message%
 git push
